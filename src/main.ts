606f4052f95e76f984cb938a7b2eb0ff814ef100
michaelgirardet <michaelgirardet@outlook.com>
2025-11-30 15:52:53 +0100

    chore(screen): adapte screen size for all screens

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import Game from "./scenes/GameScene";
+import GameScene from "./scenes/GameScene";
 import MenuScene from "./scenes/MenuScene";
 import PauseScene from "./scenes/PauseScene";
-import Phaser from "phaser";
 import { Preloader } from "./scenes/Preloader";
 import VictoryScene from "./scenes/VictoryScene";
 
@@ -10,9 +9,25 @@ new Phaser.Game({
 	width: 1440,
 	height: 800,
 	pixelArt: true,
+
+	scale: {
+		mode: Phaser.Scale.FIT, // s'adapte automatiquement
+		autoCenter: Phaser.Scale.CENTER_BOTH, // centre le jeu
+		min: {
+			width: 1024,
+			height: 576,
+		},
+		max: {
+			width: 1920,
+			height: 1080,
+		},
+		fullscreenTarget: "body",
+	},
+
 	physics: {
 		default: "arcade",
 		arcade: { fps: 60, gravity: { y: 600 }, debug: false },
 	},
-	scene: [Preloader, Game, MenuScene, PauseScene, VictoryScene],
+
+	scene: [Preloader, GameScene, MenuScene, PauseScene, VictoryScene],
 });
